b48b9f5a1ff89bba7d867d5938cdbe683e0e5088
drew <drewbeland@gmail.com>
2023-01-31 18:22:54 -0800

    figured out needed to make server.js the main. also added main.js to update html with api data

--- js/main.js
+++ js/main.js
@@ -1,1 +1,14 @@
-alert('twerkin')
+document.querySelector('button').addEventListener('click', apiRequest)
+
+async function apiRequest(){
+    const rapperName = document.querySelector('input').value
+    try{
+        const response = await fetch(`https://easy-rose-wombat-suit.cyclic.app/api/${rapperName}`)
+        const data = await response.json()
+
+        console.log(data)
+        document.querySelector('h2').innerText = data.birthName
+    }catch(error){
+        console.log(error)
+    }
+}
